3fee55daac8c47574ebaab6fdbaa68c91d0709a1
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-05-08 18:37:04 -0400

    Improves UI by removing read more button and instead makes camp cards clickable, shows no reviews yet instead of 0 comments and 0 stars, changes camp submission details paragraph, removes prev and next button and instead puts icons.

--- public/js/agency.js
+++ public/js/agency.js
@@ -1,5 +1,18 @@
 const $fileField = document.querySelector("input[type='file']");
+const $campCards = document.querySelectorAll(".card");
 
-$fileField.onchange = function () {
-	document.querySelector("input[type='file'] + label").textContent = this.files[0].name;
-};
+if ($fileField != null) {
+  $fileField.onchange = function () {
+    document.querySelector(
+      "input[type='file'] + label"
+    ).textContent = this.files[0].name;
+  };
+}
+
+if ($campCards.length) {
+  $campCards.forEach((card) => {
+    card.addEventListener("click", function () {
+      window.location.pathname = `/campgrounds/${this.id}/comments`;
+    });
+  });
+}
